382e596f29fb220839b09b8d759ee35c3e49bcd7
ssaavvaa <Toxasava@icloud.com>
2020-02-17 09:43:35 -0500

    Updates

---
+++ precache-manifest.b78ee6b019c70d08e7e2aada417609d7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47770de1f45a514d67bbf7d1f8c858b",
+    "url": "/auto-store/index.html"
+  },
+  {
+    "revision": "3796a06e524241cd0033",
+    "url": "/auto-store/static/css/main.06434829.chunk.css"
+  },
+  {
+    "revision": "65057f80709fc3986e36",
+    "url": "/auto-store/static/js/2.b199282e.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/auto-store/static/js/2.b199282e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3796a06e524241cd0033",
+    "url": "/auto-store/static/js/main.3761e9f3.chunk.js"
+  },
+  {
+    "revision": "c07f889a7c2a65c7df0d",
+    "url": "/auto-store/static/js/runtime-main.f73f5ba2.js"
+  },
+  {
+    "revision": "f3483f64fe4f53928cfe2786e7ec82cc",
+    "url": "/auto-store/static/media/FuturaPTBold.f3483f64.otf"
+  },
+  {
+    "revision": "a15258a71bcff8eb791ad54f27144d48",
+    "url": "/auto-store/static/media/FuturaPTLight.a15258a7.otf"
+  },
+  {
+    "revision": "bd99f124d37cfd9caecec4fe24094334",
+    "url": "/auto-store/static/media/FuturaPTMedium.bd99f124.otf"
+  },
+  {
+    "revision": "85169789a22defd0649ed794178588c0",
+    "url": "/auto-store/static/media/background.85169789.jpg"
+  },
+  {
+    "revision": "7d851ecb4b6e244ce88eb0bc6e845771",
+    "url": "/auto-store/static/media/engine.7d851ecb.jpg"
+  }
+]);
